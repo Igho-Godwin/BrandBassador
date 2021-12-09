04795aa7a7dc15af7a552e4ab1e7df5459195a02
Avwerosuo Igho-Godwin <avwerosuoigho-godwin@Avwerosuos-MacBook-Pro.local>
2021-12-09 19:47:14 +0100

    added ReadMe

---
+++ src/types/Feed.ts
@@ -0,0 +1,15 @@
+
+type FileSrc = {
+  alt: string;
+  src: string;
+};
+
+type Feed = {
+  title: string;
+  date: string;
+  video: FileSrc;
+  image: FileSrc;
+  cashReward: number;
+};
+
+export default Feed;
